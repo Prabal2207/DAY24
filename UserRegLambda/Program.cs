97bda958084b622d4b40f1fd704ce8a3979945bb
Prabal2207 <prabal760@gmail.com>
2021-08-09 09:21:14 +0530

    [Prabal] Added:- This is main file

---
+++ UserRegLambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace User_Registration_Day19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationValidation obj = new UserRegistrationValidation();
+
+            Console.WriteLine("Checking Validation of User Input : ");
+
+            obj.validateFirstName();
+
+        }
+    }
+}
